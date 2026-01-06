d26c49841b7e4f7c2b8420316e32be709277077c
chaebeen <tnwl05200@gmail.com>
2026-01-06 18:43:16 +0900

    add

--- main.js
+++ main.js
@@ -34,3 +34,22 @@ scrollBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+/* 모바일 */
+
+const hamburgerIcon = document.querySelector('.hamburger i');
+const mobileMenu = document.querySelector('.mobile_menu');
+const header = document.querySelector('.header');
+
+hamburgerIcon.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');   
+  header.classList.toggle('menu_open');    
+
+  if (hamburgerIcon.classList.contains('fa-bars')) {
+    hamburgerIcon.classList.remove('fa-bars');
+    hamburgerIcon.classList.add('fa-xmark');
+  } else {
+    hamburgerIcon.classList.remove('fa-xmark');
+    hamburgerIcon.classList.add('fa-bars');
+  }
+});
